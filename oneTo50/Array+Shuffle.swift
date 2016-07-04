f5994e0e3d62e5459e9ea4de8c8ac70cf498526f
Jonghyun Kim <filmhomage@hotmail.com>
2016-07-04 22:22:57 +0900

    Add to array extension

---
+++ oneTo50/Array+Shuffle.swift
@@ -0,0 +1,35 @@
+//
+//  Extension.swift
+//  oneTo50
+//
+//  Created by earth on 7/4/16.
+//  Copyright © 2016 kokaru.com. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    var shuffle:[Element] {
+        var elements = self
+        for index in 0..<elements.count {
+            let newIndex = Int(arc4random_uniform(UInt32(elements.count-index)))+index
+            if index != newIndex {
+                swap(&elements[index], &elements[newIndex])
+            }
+        }
+        return elements
+    }
+    func groupOf(n:Int)-> [[Element]] {
+        var result:[[Element]]=[]
+        for i in 0...(count/n)-1 {
+            var tempArray:[Element] = []
+            for index in 0...n-1 {
+                tempArray.append(self[index+(i*n)])
+            }
+            result.append(tempArray)
+        }
+        
+        return result
+    }
+}
